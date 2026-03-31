e09790620e688d9b39e7e18842fca6ede5a91ad5
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-31 16:05:40 +0530

    add protect middleware on crud controllers routes

--- sms-backend/src/routes/home.routes.js
+++ sms-backend/src/routes/home.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { addStudent, getAllStudent, updateStudent, deleteStudent } from "../controllers/crudOperations.controller.js";
+import protect from "../middlewares/auth.middleware.js"
 
 const operationsRouter = express.Router();
 
 
-operationsRouter.get("/", getAllStudent);
-operationsRouter.post("/", addStudent);
-operationsRouter.patch("/:id", updateStudent);
-operationsRouter.delete("/:id", deleteStudent);
+operationsRouter.get("/", protect, getAllStudent);
+operationsRouter.post("/", protect, addStudent);
+operationsRouter.patch("/:id", protect, updateStudent);
+operationsRouter.delete("/:id", protect, deleteStudent);
 
 export default operationsRouter;
