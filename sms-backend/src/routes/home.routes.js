dbc9436b2139f98404372b79dacdd4dcee93bfbd
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-31 15:55:32 +0530

    add routes for the crud operations controller

--- sms-backend/src/routes/home.routes.js
+++ sms-backend/src/routes/home.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { addStudent } from "../controllers/crudOperations.controller.js";
+import { addStudent, getAllStudent, updateStudent, deleteStudent } from "../controllers/crudOperations.controller.js";
 
 const operationsRouter = express.Router();
 
 
+operationsRouter.get("/", getAllStudent);
 operationsRouter.post("/", addStudent);
+operationsRouter.patch("/:id", updateStudent);
+operationsRouter.delete("/:id", deleteStudent);
 
 export default operationsRouter;
