fed803e47d03ed1d55e4f4453655de0ea615f6d0
Rudresh <bamaniarudresh2008@gmail.com>
2026-04-01 08:01:43 +0530

    fix the baseUrl

--- sms-frontend/src/api/api.js
+++ sms-frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "http://localhost:7000/"});
+const API = axios.create({baseURL: "http://localhost:7000/api/"});
 
 API.interceptors.request.use((config) => {
     const token = JSON.parse(localStorage.getItem("sms-token"));    
