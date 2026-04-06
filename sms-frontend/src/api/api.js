376125f33ed2f5faeaca5898f975126b8c5a26eb
Rudresh <bamaniarudresh2008@gmail.com>
2026-04-06 13:43:30 +0530

    added main backend api link to baseUrl on axios wrapper

--- sms-frontend/src/api/api.js
+++ sms-frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "http://localhost:7000/api/"});
+const API = axios.create({baseURL: "https://full-stack-sms-project.onrender.com/api"});
 
 API.interceptors.request.use((config) => {
     const token = JSON.parse(localStorage.getItem("sms-token"));    
